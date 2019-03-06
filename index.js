cce1e9b595eb2b87c5aa5b3de6f1b191f0f60a9f
Blake A Long <blake.a.long@gmail.com>
2019-03-06 03:11:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,13 +15,9 @@ var logWhisper = (string) => {
 };
 
 var sayHiToGrandma = (string) => {
-  if (string === string.toLowerCase()) {
-    return "I can't hear you!";
-  } else if (string === string.toUpperCase()) {
-    return "YES INDEED!";
-  } else if (string === "I love you, Grandma.") {
-    return "I love you, too.";
+  switch(string) {
+    case string = string.toLowerCase():
+      return "I can't hear you"
   }
-};
 
 
